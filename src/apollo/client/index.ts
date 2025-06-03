606fb8260c653c4046f9589639ecddef6d1e4295
nikitastas <nikitastas1694@gmail.com>
2025-06-03 13:38:02 +0300

    SCRUM-128 feat: add the ability to use GraphQL Subscriptions

--- src/apollo/client/index.ts
+++ src/apollo/client/index.ts
@@ -1,4 +1,7 @@
-import {ApolloClient, InMemoryCache, makeVar, gql} from "@apollo/client";
+import {ApolloClient, InMemoryCache, makeVar, gql, HttpLink, split} from "@apollo/client";
+import {getMainDefinition} from "@apollo/client/utilities";
+import {createClient} from "graphql-ws";
+import {GraphQLWsLink} from "@apollo/client/link/subscriptions";
 
 export const isLoggedInVar = makeVar(false);
 
@@ -23,9 +26,38 @@ export const cache = new InMemoryCache({
   }
 });
 
-export const client = new ApolloClient({
+// Создание HTTP-линка
+const httpLink = new HttpLink({
   headers: { authorization: 'Basic [YWRtaW5AZ21haWwuY29tOmFkbWlu]' },
   uri: "https://inctagram.work/api/v1/graphql",
+});
+
+// Создание WebSocket-клиента с помощью graphql-ws
+const wsClient = createClient({
+  url: "wss://inctagram.work/api/v1/graphql", // WebSocket URL
+  connectionParams: {
+    authorization: 'Basic [YWRtaW5AZ21haWwuY29tOmFkbWlu]', // Передача токена авторизации
+  },
+});
+
+// Создание WebSocket-линка
+const wsLink = new GraphQLWsLink(wsClient);
+
+// Разделение запросов между HTTP и WebSocket
+const splitLink = split(
+  ({ query }) => {
+    const definition = getMainDefinition(query);
+    return (
+      definition.kind === "OperationDefinition" &&
+      definition.operation === "subscription"
+    );
+  },
+  wsLink, // Для подписок
+  httpLink // Для запросов и мутаций
+);
+
+export const client = new ApolloClient({
+  link: splitLink,
   cache: cache,
 });
 
