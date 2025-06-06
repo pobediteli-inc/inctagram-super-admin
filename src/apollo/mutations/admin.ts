e4f71bde8c8c7f36f6ce854d84728063a9d55df1
yukimurochka <uryc0n@yandex.ru>
2025-06-06 19:13:42 +0300

    SCRUM-130 feat: add dropdown for change user status, delete user modal

--- src/apollo/mutations/admin.ts
+++ src/apollo/mutations/admin.ts
@@ -1,11 +1,15 @@
-import { gql } from '@apollo/client';
+import { gql } from "@apollo/client";
 
 export const LOGIN_ADMIN = gql`
   mutation LoginAdmin($email: String!, $password: String!) {
-    loginAdmin(
-      email: $email,
-      password: $password
-    ) {
+    loginAdmin(email: $email, password: $password) {
       logged
     }
-  }`
+  }
+`;
+
+export const REMOVE_USER = gql`
+  mutation RemoveUser($userId: Int!) {
+    removeUser(userId: $userId)
+  }
+`;
