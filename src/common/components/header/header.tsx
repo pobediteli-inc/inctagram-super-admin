b2d3453ff68a333472d309f402e42b4556f6559f
umedjan kabiri <umedjan.kabiri@gmail.com>
2025-06-09 13:12:49 +0300

    SCRUM-131 refactor: Update Header component logic, improve state handling, and enhance logo styling

--- src/common/components/header/header.tsx
+++ src/common/components/header/header.tsx
@@ -4,60 +4,62 @@ import { FC, useEffect } from "react";
 import s from "./header.module.css";
 import { Select } from "common/components/select/select";
 import { Typography } from "common/components/typography/typography";
-import { Button } from "common/components/button/button";
-import Link from "next/link";
 import { FlagRussia, FlagUnitedKingdom } from "assets/icons";
 import { SelectItemsProps } from "common/types/SelectItemsProps/SelectItemsProps";
 import { LogOut } from "common/components/logOut/logOut";
-import { authApi, useMeQuery } from "store/services/api/auth";
 import { useAppSelector } from "common/hooks/useAppSelector";
-import { selectIsLoggedIn, setLoggedIn } from "store/services/slices/authSlice";
+import { selectIsLoggedIn, selectStatus, setLoggedIn, setStatus } from "store/services/slices";
 import { useAppDispatch } from "common/hooks/useAppDispatch";
 import { handleErrors } from "common/utils/handleErrors";
-import { usePathname, useRouter } from "next/navigation";
+import { useRouter } from "next/navigation";
+import { client, isLoggedInVar, updateLoginState } from "apollo/client";
+import { useReactiveVar } from "@apollo/client";
 
 export const Header: FC = () => {
-  const { data, isLoading } = useMeQuery();
+  const authStatus = useReactiveVar(isLoggedInVar);
   const isLoggedIn = useAppSelector(selectIsLoggedIn);
+  const { status: isLoading } = useAppSelector(selectStatus);
   const dispatch = useAppDispatch();
   const router = useRouter();
-  const pathname = usePathname();
-
-  const { email } = data ?? {};
 
   const selectLanguages: SelectItemsProps[] = [
     { value: "en", label: "English", icon: <FlagUnitedKingdom width={20} height={20} /> },
     { value: "ru", label: "Russian", icon: <FlagRussia width={20} height={20} /> },
   ];
 
-  const handleLogOut = () => {
-    localStorage.removeItem("accessToken");
-    dispatch(authApi.util.resetApiState());
+  const handleLogOut = async () => {
     dispatch(setLoggedIn({ isLoggedIn: false }));
+    updateLoginState(false);
   };
   const handleOnMainPage = () => router.push("/");
 
   useEffect(() => {
     try {
-      if (data) dispatch(setLoggedIn({ isLoggedIn: true }));
+      if (authStatus) {
+        dispatch(setLoggedIn({ isLoggedIn: true }));
+        dispatch(setStatus({ status: null, message: null }))
+      }
     } catch (error: unknown) {
       handleErrors(error, dispatch);
       dispatch(setLoggedIn({ isLoggedIn: false }));
+      client.clearStore();
     }
-  }, [data, isLoggedIn, dispatch]);
+  }, [authStatus, dispatch]);
 
   return (
     <header className={s.headerWrapper}>
       <div className={s.mainWrapper}>
-        <Typography
-          style={{ cursor: "pointer" }}
-          variant={"large"}
-          color={"light"}
-          textAlign={"center"}
-          onClick={handleOnMainPage}
-        >
-          InctagramSuperAdmin
-        </Typography>
+        <div className={s.logo}>
+          <Typography
+            style={{ cursor: "pointer" }}
+            variant={"large"}
+            color={"light"}
+            textAlign={"center"}
+            onClick={handleOnMainPage}
+          >Inctagram</Typography>
+          <Typography variant={"small"}>Super</Typography>
+          <Typography variant={"bold_small"}>Admin</Typography>
+        </div>
         <div className={s.selectButtonsWrapper}>
           <Select defaultValue={"en"} items={selectLanguages} groupLabel={"Languages"} />
           <div className={s.buttonsWrapper}>
@@ -65,20 +67,9 @@ export const Header: FC = () => {
               <>
                 <Typography variant={"regular_14"}>Loading...</Typography>
               </>
-            ) : isLoggedIn ? (
-              <LogOut onLogOutAction={handleLogOut} email={email ?? null} />
-            ) : !isLoggedIn && (pathname === "/login" || pathname === "/auth") ? (
-              <></>
-            ) : (
-              <>
-                <Button variant={"link"} asChild>
-                  <Link href={"/login"}>Log in</Link>
-                </Button>
-                <Button variant={"primary"} asChild>
-                  <Link href={"/auth"}>Sign up</Link>
-                </Button>
-              </>
-            )}
+            ) : isLoggedIn ?
+              <LogOut onLogOutAction={handleLogOut} email={"admin@gmail.com"} /> : <></>
+            }
           </div>
         </div>
       </div>
