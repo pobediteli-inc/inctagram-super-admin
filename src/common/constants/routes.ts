a96038f97cc045cff0d4ea46cffac9f8ce3f405f
doroshkent <doroshkent@gmail.com>
2025-06-19 17:20:48 +0200

    Add link to public user profile in user details page
    Replaced static username display with a clickable link leading to the public user profile. Updated styling for the username and added a new route for public user profiles in the constants file.

--- src/common/constants/routes.ts
+++ src/common/constants/routes.ts
@@ -2,4 +2,5 @@ export const ROUTES = {
   user: (userId: number) => `/user/${userId}`,
   usersList: "/users-list",
   signInAdmin: "/sign-in-admin",
+  publicUser: (userId: number) => `/public-user/${userId}`,
 } as const;
